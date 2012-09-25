55b6766bec3361259e988b8fdafb7aad9204d79e
Spencer Gibb <spencer@gibb.us>
2012-09-25 00:01:27 -0600

    moved scala-example to separate module

--- scala_2.9.1/src/test/scala/thirtytwo/degrees/halfpipe/HalfpipeConsole.scala
+++ scala_2.9.1/src/test/scala/thirtytwo/degrees/halfpipe/HalfpipeConsole.scala
@@ -1,16 +1,14 @@
 package thirtytwo.degrees.halfpipe
 
-import scalaexample.resources.HelloResource
-import thirtytwo.degrees.halfpipe.scalaexample.AppConfig
+//import thirtytwo.degrees.halfpipe.scalaexample.AppConfig
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
-import tools.nsc.MainGenericRunner
 import tools.nsc.interpreter.ILoop._
 
 object HalfpipeConsole {
   def main(args: Array[String]) {
-    val ctx = new AnnotationConfigApplicationContext(classOf[AppConfig])
-    val hello = ctx.getBean(classOf[HelloResource])
-    println(hello.hellotext(new Some[String](" here is more")))
+    //val ctx = new AnnotationConfigApplicationContext(classOf[AppConfig])
+    //val hello = ctx.getBean(classOf[HelloResource])
+    //println(hello.hellotext(new Some[String](" here is more")))
     break(Nil)
     /*MainGenericRunner.main(args)
     sys.exit(0)*/

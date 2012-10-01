68ddf3a562f3e591df220cffe3bb5a3bd84c84ef
Spencer Gibb <spencer@gibb.us>
2012-09-30 18:15:16 -0600

    fixed expand dir, find war in .halfpipedir

--- core/src/main/java/thirtytwo/degrees/halfpipe/cli/ServerCommand.java
+++ core/src/main/java/thirtytwo/degrees/halfpipe/cli/ServerCommand.java
@@ -1,26 +1,19 @@
 package thirtytwo.degrees.halfpipe.cli;
 
-import static org.apache.tomcat.maven.runner.Tomcat7RunnerCli.STAND_ALONE_PROPERTIES_FILENAME;
+import static com.google.common.collect.Iterables.*;
 
-import static com.google.common.collect.Iterators.*;
-import static com.google.common.base.Predicates.*;
-
-import com.google.common.base.Optional;
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.*;
-import org.apache.tomcat.JarScanner;
 import org.apache.tomcat.JarScannerCallback;
-import org.apache.tomcat.maven.runner.Tomcat7Runner;
 import org.apache.tomcat.util.scan.StandardJarScanner;
 
 import javax.servlet.ServletContext;
-import java.io.*;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.*;
+import java.io.File;
+import java.util.Collection;
+import java.util.Set;
 
 /**
  * User: spencergibb
@@ -37,20 +30,22 @@ public class ServerCommand extends Command {
     public void run(CommandLine commandLine) throws Exception {
         System.out.println("Starting Server");
 
-        Properties sysprops = System.getProperties();
-        Optional<String> propname = tryFind(forEnumeration((Enumeration<String>) sysprops.propertyNames()), containsPattern("one-jar"));
-
-        if (propname.isPresent()) {
+        if (isOneJar()) {
             System.out.println("in one-jar");
+
+            File warFile = findWarFile();
+
+            String baseDir = warFile.getAbsolutePath();
+            //String baseDir = userDir+File.separator+".halfpipe/binlibs/halfpipe-example.war";
+            System.out.println("baseDir: " + baseDir);
+
             Tomcat tomcat = new Tomcat();
-            String userDir = System.getProperty("user.dir");
-            String baseDir = userDir+File.separator+".halfpipe/binlibs/halfpipe-example.war";
             Context context = tomcat.addWebapp("", baseDir);
             System.setProperty("tomcat.util.scan.DefaultJarScanner.jarsToSkip", "*.jar");
-            StandardJarScanner jarScanner = new StandardJarScanner(){
+            StandardJarScanner jarScanner = new StandardJarScanner() {
                 @Override
                 public void scan(ServletContext context, ClassLoader classloader, JarScannerCallback callback, Set<String> jarsToSkip) {
-                    System.out.println("scanning classloader: "+classloader);
+                    System.out.println("scanning classloader: " + classloader);
                     super.scan(context, classloader, callback, jarsToSkip); //TODO: implement .scan
                 }
             };
@@ -59,42 +54,17 @@ public class ServerCommand extends Command {
             context.setReloadable(false);
             //https://github.com/grails/grails-core/blob/master/grails-plugin-tomcat/src/main/groovy/org/grails/plugins/tomcat/InlineExplodedTomcatServer.groovy
             ClassLoader classLoader = getClass().getClassLoader();
-            System.out.println("setting classloader: "+classLoader);
+            System.out.println("setting classloader: " + classLoader);
             TomcatLoader loader = new TomcatLoader(classLoader);
             loader.setContainer(context);
             context.setLoader(loader);
             tomcat.start();
             waitIndefinitely();
 
-        /*Properties properties = buildStandaloneProperties();
-        if (properties != null) {
-            System.out.println("starting exec jar");
-            Tomcat7Runner runner = new Tomcat7Runner();
-            runner.runtimeProperties = properties;
-            runner.run();*/
         } else {
             System.out.println("starting dev");
-            String userDir = System.getProperty("user.dir");
-            AndFileFilter filter = new AndFileFilter();
-            filter.addFileFilter(new NameFileFilter("web.xml"));
-            filter.addFileFilter(new AbstractFileFilter() {
-                public boolean accept(File file) {
-                    String path = file.getAbsolutePath();
-                    return path.matches(".*target.*WEB-INF.*") && !path.matches(".*war.work.*");
-                }
-            });
-            Collection<File> files = FileUtils.listFiles(new File(userDir), filter, TrueFileFilter.INSTANCE);
-            if (files.isEmpty()) {
-                System.err.println("No directory!");
-                System.exit(1);
-            }
-            if (files.size() > 1) {
-                System.err.println("More than one file! "+files);
-            }
+            String baseDir = getWebappDir();
             Tomcat tomcat = new Tomcat();
-            File file = files.iterator().next();
-            String baseDir = file.getParentFile().getParentFile().getAbsolutePath();
-            //String baseDir = userDir+File.separator+"src"+File.separator+"main"+File.separator+"webapp";
             tomcat.addWebapp("", baseDir);
             tomcat.start();
             waitIndefinitely();
@@ -102,37 +72,63 @@ public class ServerCommand extends Command {
         }
     }
 
+    private String getWebappDir() {
+        String userDir = System.getProperty("user.dir");
+        AndFileFilter filter = new AndFileFilter();
+        filter.addFileFilter(new NameFileFilter("web.xml"));
+        filter.addFileFilter(new AbstractFileFilter() {
+            public boolean accept(File file) {
+                String path = file.getAbsolutePath();
+                return path.matches(".*target.*WEB-INF.*") && !path.matches(".*war.work.*");
+            }
+        });
+        Collection<File> files = FileUtils.listFiles(new File(userDir), filter, TrueFileFilter.INSTANCE);
+        if (files.isEmpty()) {
+            System.err.println("No directory!");
+            System.exit(1);
+        }
+        if (files.size() > 1) {
+            System.err.println("More than one file! " + files);
+        }
+        File file = files.iterator().next();
+        //String baseDir = userDir+File.separator+"src"+File.separator+"main"+File.separator+"webapp";
+        return file.getParentFile().getParentFile().getAbsolutePath();
+    }
 
-    private static void waitIndefinitely()
-    {
+    private File findWarFile() {
+        String userDir = System.getProperty("user.dir") + File.separator + ".halfpipe";
+        Collection<File> files = FileUtils.listFiles(new File(userDir), new RegexFileFilter(".*\\.war"), TrueFileFilter.INSTANCE);
+        if (files.isEmpty()) {
+            System.err.println("No directory!");
+            System.exit(1);
+        }
+        if (files.size() > 1) {
+            System.err.println("More than one file! " + files);
+        }
+        return getFirst(files, null);
+    }
+
+    private boolean isOneJar() {
+        Class<?> klass = null;
+        try {
+            klass = Class.forName("OneJar");
+        } catch (ClassNotFoundException e) { /*ignore*/ }
+
+        return klass != null;
+    }
+
+
+    private static void waitIndefinitely() {
         Object lock = new Object();
 
-        synchronized ( lock )
-        {
-            try
-            {
+        synchronized (lock) {
+            try {
                 lock.wait();
-            }
-            catch ( InterruptedException exception )
-            {
-                throw new Error( "InterruptedException on wait Indefinitely lock:" + exception.getMessage(),
-                        exception );
+            } catch (InterruptedException exception) {
+                throw new Error("InterruptedException on wait Indefinitely lock:" + exception.getMessage(),
+                        exception);
             }
         }
     }
-
-/*
-    private static Properties buildStandaloneProperties()
-            throws IOException {
-        InputStream is =
-                Thread.currentThread().getContextClassLoader().getResourceAsStream(STAND_ALONE_PROPERTIES_FILENAME);
-        if (is == null)
-            return null;
-        Properties properties = new Properties();
-        properties.load(is);
-        return properties;
-    }
-*/
-
 
 }

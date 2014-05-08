56519dcb0e5d100bf4464dc7aa623744ab3c4f9a
Spencer Gibb <spencer.gibb@domo.com>
2014-05-08 13:34:40 -0600

    moved ansible roles to roles directory

--- dev/docker/docker_consul_server.sh
+++ dev/docker/docker_consul_server.sh
@@ -1,1 +1,2 @@
-docker run -d --dns=172.17.42.1 --name=consulserver1 32degrees/consulserver:0.1 /sbin/my_init --enable-insecure-key
+#docker run -d --net=host --dns=10.1.42.1 --name=consulserver1 32degrees/consulserver:0.1 /sbin/my_init --enable-insecure-key
+docker run -d --name=consulserver1 32degrees/consulserver:0.1 /sbin/my_init --enable-insecure-key
--- dev/docker/docker_skydns.sh
+++ dev/docker/docker_skydns.sh
@@ -1,2 +1,2 @@
-docker run -d -p 172.17.42.1:53:53/udp --name skydns crosbymichael/skydns -nameserver 8.8.8.8:53 -domain halfpipe
+docker run --net=host -d -p 10.1.42.1:53:53/udp --name skydns crosbymichael/skydns -nameserver 8.8.8.8:53 -domain halfpipe
 
